820157414f47000e6f4ef3258d74e9711fa07fb7
d-sqrd <deb.bunny98@gmail.com>
2024-12-30 14:34:35 +0530

    fixed build errors

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -9,32 +9,6 @@ const AppProvider = ({ children }) => {
   const [taskList, setTaskList] = useState([]);
   const [userEmail, setUserEmail] = useState("");
 
-  useEffect(() => {
-    const initilizeAppOnPageRefresh = () => {
-      // This utility function is invoked whenever the page loads for the first time or page has been refreshed after user has logged-in and is used to initialize global app states like logged-in user's email, associated task-list, etc.
-      // The function basically checks the last element in the "loginCreds" localStorage key since whenever an user logs-in/logs-out to the app, his/her login creds are pushed at the end of "loginCreds" localStorage key
-      const storedLoginCreds = JSON.parse(
-        localStorage.getItem("loginCreds") || "[]"
-      );
-      if (storedLoginCreds && storedLoginCreds.length) {
-        const currLoginUserCreds =
-          storedLoginCreds[storedLoginCreds.length - 1];
-        const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-        if (currLoginUserCreds.isUserLoggedIn) {
-          setUserLogin();
-          setUserEmail(currLoginUserCreds.email);
-          const currUserStoredTasks = storedTasks.filter(
-            (task) => task.user.email === currLoginUserCreds.email
-          );
-          if (currUserStoredTasks && currUserStoredTasks.length) {
-            updateTaskList(currUserStoredTasks[0].tasks);
-          }
-        }
-      }
-    };
-
-    initilizeAppOnPageRefresh();
-  }, []);
   const setUserLogout = () => {
     pushUserCredToEndOfList(userEmail, false);
     setIsUserLoggedIn(false);
@@ -64,6 +38,33 @@ const AppProvider = ({ children }) => {
   const hideNewTaskModal = () => {
     setIsNewTaskModalVisible(false);
   };
+
+  useEffect(() => {
+    const initilizeAppOnPageRefresh = () => {
+      // This utility function is invoked whenever the page loads for the first time or page has been refreshed after user has logged-in and is used to initialize global app states like logged-in user's email, associated task-list, etc.
+      // The function basically checks the last element in the "loginCreds" localStorage key since whenever an user logs-in/logs-out to the app, his/her login creds are pushed at the end of "loginCreds" localStorage key
+      const storedLoginCreds = JSON.parse(
+        localStorage.getItem("loginCreds") || "[]"
+      );
+      if (storedLoginCreds && storedLoginCreds.length) {
+        const currLoginUserCreds =
+          storedLoginCreds[storedLoginCreds.length - 1];
+        const storedTasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+        if (currLoginUserCreds.isUserLoggedIn) {
+          setUserLogin();
+          setUserEmail(currLoginUserCreds.email);
+          const currUserStoredTasks = storedTasks.filter(
+            (task) => task.user.email === currLoginUserCreds.email
+          );
+          if (currUserStoredTasks && currUserStoredTasks.length) {
+            updateTaskList(currUserStoredTasks[0].tasks);
+          }
+        }
+      }
+    };
+
+    initilizeAppOnPageRefresh();
+  });
 
   return (
     <AppContext.Provider
